1de4317048e6a05f62b409c65607f72892335b5a
AntiKippi <24373791+AntiKippi@users.noreply.github.com>
2023-09-15 05:51:53 +0200

    Before html conversation logic rollback

--- ffa-bookmarks.py
+++ ffa-bookmarks.py
@@ -9,11 +9,13 @@
 ######################################################
 
 import argparse
+import json
 import os
 import sqlite3
 import tempfile
 from adb_shell.adb_device import AdbDeviceTcp, AdbDeviceUsb
 from adb_shell.auth.sign_pythonrsa import PythonRSASigner
+from contextlib import closing
 from enum import StrEnum, auto
 
 
@@ -22,25 +24,118 @@ class Format(StrEnum):
     JSON = auto()
 
 VALID_FORMATS = [f.value for f in Format]
-DEFAULT_FORMAT = Format.HTML
+DEFAULT_FORMAT = Format.JSON
+
 DB_FILE_NAME = 'places.sqlite'
 ANDROID_TEMP_DIR = '/data/local/tmp'
 DB_TEMP_FILE = f'{ANDROID_TEMP_DIR}/{DB_FILE_NAME}'
 
+BASE_QUERY = 'SELECT mb.guid, mb.title, mb.position, mb.dateAdded, mb.lastModified, mb.id, mb.type, mp.url FROM moz_bookmarks mb LEFT OUTER JOIN moz_places mp ON mb.fk = mp.id'
+ROOT_QUERY = f'{BASE_QUERY} WHERE mb.parent IS NULL'
+CHILDREN_QUERY = f'{BASE_QUERY} WHERE mb.parent = ?'
+
+TYPE_LOOKUP = {
+    1: 'text/x-moz-place',
+    2: 'text/x-moz-place-container'
+}
+
+ROOT = [
+    'root________',
+    'menu________',
+    'toolbar_____',
+    'unfiled_____',
+    'mobile______'
+]
+
+ROOT_LOOKUP = {
+    ROOT[0]: 'placesRoot',
+    ROOT[1]: 'bookmarksMenuFolder',
+    ROOT[2]: 'toolbarFolder',
+    ROOT[3]: 'unfiledBookmarksFolder',
+    ROOT[4]: 'mobileFolder'
+}
+
+HTML_HEAD = """<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<!-- This is an automatically generated file.
+     It will be read and overwritten.
+     DO NOT EDIT! -->
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<meta http-equiv="Content-Security-Policy"
+      content="default-src 'self'; script-src 'none'; img-src data: *; object-src 'none'"></meta>
+<TITLE>Bookmarks</TITLE>"""
+
+
+def to_html(bookmarks):
+    def add_node(folder):
+        html_node = ''
+        if folder['children']:
+            for child in folder['children']:
+                html_node += add_node(child)
+
+    bookmark_menu = [node for node in bookmarks['children'] if node['guid'] == ROOT[1]]
+    bookmark_toolbar = [node for node in bookmarks['children'] if node['guid'] == ROOT[2]]
+    bookmark_other = [node for node in bookmarks['children'] if node['guid'] == ROOT[3]]
+    rest = [node for node in bookmarks['children'] if node['guid'] != ROOT[1] and node['guid'] != ROOT[2] and node['guid'] != ROOT[3]]
+
+    html = f"""{HTML_HEAD}
+<H1>Bookmarks Menu</H1>
+
+<DL><p>
+    {''.join([add_node(child) for child in bookmark_menu['children']])}
+    <DT><H3 ADD_DATE="{bookmark_toolbar['dateAdded'] >> 3}" LAST_MODIFIED="{bookmark_toolbar['lastModified'] >> 3}" PERSONAL_TOOLBAR_FOLDER="true">Bookmarks Toolbar</H3>
+    <DL><p>
+        {''.join([add_node(child) for child in bookmark_toolbar['children']])}
+    </DL><p>
+    <DT><H3 ADD_DATE="{bookmark_other['dateAdded'] >> 3}" LAST_MODIFIED="{bookmark_other['lastModified'] >> 3}" UNFILED_BOOKMARKS_FOLDER="true">Other Bookmarks</H3>
+    <DL><p>
+        {''.join([add_node(child) for child in bookmark_other['children']])}
+    </DL><p>
+</DL>"""
+
+
+def process_node(conn, node):
+    node_dict = {
+        'guid': node[0],
+        'title': node[1],
+        'index': node[2],
+        'dateAdded': node[3] << 3,
+        'lastModified': node[4] << 3,
+        'id': node[5],
+        'typeCode': node[6],
+        'type': TYPE_LOOKUP[node[6]]
+    }
+
+    with closing(conn.cursor()) as cursor:
+        with closing(cursor.execute(CHILDREN_QUERY, (node[5],))) as res:
+            children = res.fetchall()
+            children_nodes = [process_node(conn, n) for n in children]
+
+    if node[0] in ROOT_LOOKUP:
+        node_dict['root'] = ROOT_LOOKUP[node[0]]
+
+    if node[7] is not None:
+        node_dict['uri'] = node[7]
+
+    if children_nodes:
+        node_dict['children'] = children_nodes
+
+    return node_dict
+
 
 def set_fileformat(filename, fformat):
-    if fformat is None:
+    if fformat:
+        return Format(fformat)
+    else:
         ext = filename.split('.')[-1]
         return Format(ext) if ext in VALID_FORMATS else DEFAULT_FORMAT
-    else:
-        return Format(fformat)
 
 
 def main():
     ff_package_name = "org.mozilla.firefox"
+    fileformat = None
     infile = None
     outfile = None
-    fileformat = None
+    dbfile = None
     privkey = os.path.join(os.path.expanduser('~'), '.android', 'adbkey')
     pubkey = f'{privkey}.pub'
 
@@ -62,6 +157,14 @@ def main():
                         default=fileformat,
                         choices=VALID_FORMATS,
                         help=f'Specify the output format. If omitted, the format is determined from the outfile extension and falls back to {DEFAULT_FORMAT}.')
+    parser.add_argument('-d',
+                        '--db-file',
+                        type=str,
+                        action='store',
+                        dest='dbfile',
+                        required=False,
+                        default=dbfile,
+                        help=f'Use this sqlite db instead of fetching it from the device')
     parser.add_argument('--public-key',
                         type=str,
                         action='store',
@@ -96,46 +199,68 @@ def main():
 
     args = parser.parse_args()
     ff_package_name = args.ff_package_name
+    fileformat = set_fileformat(infile or outfile or '', args.fformat)
     infile = args.infile
     outfile = args.outfile
-    fileformat = set_fileformat(infile or outfile, args.fformat)
+    dbfile = args.dbfile
     pubkey = args.pubkey
     privkey = args.privkey
 
+    if dbfile is None:
+        # Load the public and private keys
+        with open(privkey) as f:
+            priv = f.read()
+        with open(pubkey) as f:
+            pub = f.read()
+        signer = PythonRSASigner(pub, priv)
 
-    # Load the public and private keys
-    with open(privkey) as f:
-        priv = f.read()
-    with open(pubkey) as f:
-        pub = f.read()
-    signer = PythonRSASigner(pub, priv)
+        # Connect via USB
+        device = AdbDeviceUsb()
+        device.connect(rsa_keys=[signer])
 
-    # Connect via USB
-    device = AdbDeviceUsb()
-    device.connect(rsa_keys=[signer])
+        # Copy db into tmp
+        get_db_commands = f'cp \'\\\'\'/data/data/{ff_package_name}/files/{DB_FILE_NAME}\'\\\'\' \'\\\'\'{DB_TEMP_FILE}\'\\\'\';' + \
+                          f'chown shell:shell \'\\\'\'{DB_TEMP_FILE}\'\\\'\';'
 
-    # Copy db into tmp
-    get_db_commands = f'cp \'\\\'\'/data/data/{ff_package_name}/files/{DB_FILE_NAME}\'\\\'\' \'\\\'\'{DB_TEMP_FILE}\'\\\'\';' + \
-                      f'chown shell:shell \'\\\'\'{DB_TEMP_FILE}\'\\\'\';'
+        print(device.shell(f'su -c \'{get_db_commands}\''))
 
-    print(device.shell(f'su -c \'{get_db_commands}\''))
+        # Copy db to host and open it
+        tmpdir = tempfile.TemporaryDirectory()
+        dbfile = os.path.join(tmpdir.name, DB_FILE_NAME)
+        device.pull(DB_TEMP_FILE, dbfile)
 
-    # Copy db to host and open it
-    tmpdir = tempfile.TemporaryDirectory()
-    dbfile = os.path.join(tmpdir.name, DB_FILE_NAME)
-    device.pull(DB_TEMP_FILE, dbfile)
-    conn = sqlite3.connect(dbfile)
-    cur = conn.cursor()
+    with sqlite3.connect(dbfile) as conn:
+        with closing(conn.cursor()) as cur:
 
-    # The export flag has been used
-    if outfile is not None:
-        print(dbfile)
-    # The import flag has been used
-    elif infile is not None:
-        print(dbfile)
-    # This should not happen
-    else:
-        raise RuntimeError('No export or import flag given.')
+            # The export flag has been used
+            if outfile is not None:
+                # Get the root node and build a bookmarks object
+                with closing(cur.execute(ROOT_QUERY)) as res:
+                    bookmarks = process_node(conn, res.fetchone())
+
+                bookmarks_out = ''
+                if fileformat == Format.JSON:
+                    bookmark_out = json.dumps(bookmarks)
+                elif fileformat == Format.HTML:
+                    # Convert to HTML
+                    bookmarks_out = 'HTML'
+                else:
+                    raise RuntimeError('No valid format given.')
+
+                # Output the exported bookmarks
+                if outfile == '':
+                    print(bookmarks_out)
+                else:
+                    with open(outfile, 'w') as ofile:
+                        ofile.write(bookmarks_out)
+
+            # The import flag has been used
+            elif infile is not None:
+                print(dbfile)
+
+            # This should not happen
+            else:
+                raise RuntimeError('No export or import flag given.')
 
 
 
